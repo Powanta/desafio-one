aa965afd1cc82b42bbbdbf3954db2585bc3abdb4
Powanta <powanta@gmail.com>
2025-04-03 20:49:32 -0300

    Desafio One Dio Classificador de Xp de Heroi

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-asdf
+let nomeDeHeroi = ""
+let xpDeHeroi = 2
+let classificacao = ""
+
+    if (xpDeHeroi <= 1000) {
+        classificacao = "Ferro"
+    }
+    else if (xpDeHeroi >= 1001 && xpDeHeroi <= 2000) {
+        classificacao = "Bronze"
+    }
+    else if (xpDeHeroi >= 2001 && xpDeHeroi <= 5000){
+        classificacao = "Prata-Ouro"
+    }
+    else if (xpDeHeroi >= 5001 && xpDeHeroi <= 9000) {
+        classificacao = "Platina-Diamante"
+    }
+    else if (xpDeHeroi >= 9001 && xpDeHeroi <= 10000) {
+        classificacao = "Ascendente"
+    }
+    else classificacao = "Radiante"
+
+    console.log("O herói " + nomeDeHeroi + "está classificado como " + classificacao)
